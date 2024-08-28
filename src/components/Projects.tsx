f1b3d60d05bffbc6de74ea430304e25fd54de709
Oghenefegor Idoro <69634999+fegig@users.noreply.github.com>
2024-08-28 19:37:33 +0100

    added project quote

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -6,8 +6,13 @@ export default function Projects() {
     <div className="flex  justify-evenly items-center mx-auto my-6 flex-wrap gap-x-2 gap-y-4 max-w-screen-xl py-4">
       <div className='flex-col flex items-center'>
       <div className="tracking-wider text-center lg:text-left text-6xl font-bold ">Recent Projects</div>
-    <div className='text-center my-2'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex quod veritatis praesentium pariatur accusantium sapiente ea ipsam vero. Labore illo ipsam cupiditate assumenda iste ullam beatae sint animi quasi qui.</div>
-      </div>
+      <div className='text-center my-2 text-lg'>
+        <blockquote className='italic'>
+        "Innovation distinguishes between a leader and a follower." — <span className='text-primary-alt'>Steve Jobs</span>
+        </blockquote>
+
+</div>   
+</div>
   
       </div>
     </section>
